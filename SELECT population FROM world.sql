97d5cc1649526f824250f3416173f2a2fadaa004
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-18 22:46:04 -0600

    Add the join operation

--- SELECT population FROM world.sql
+++ SELECT population FROM world.sql
@@ -300,3 +300,72 @@ SELECT continent FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000;
 
+--The JOIN operation
+--1
+SELECT matchid, player FROM goal 
+WHERE teamid = 'GER';
+
+--2
+SELECT id,stadium,team1,team2
+  FROM game
+  WHERE id = 1012;
+
+--3
+SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+  WHERE teamid = 'GER';
+
+--4
+SELECT team1, team2, player
+  FROM game JOIN goal ON (id=matchid)
+  WHERE player LIKE 'Mario%'
+
+--5
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam on teamid = id
+ WHERE gtime<=10
+
+--6
+SELECT mdate, teamname FROM game JOIN eteam ON (team1 = eteam.id)
+ WHERE coach LIKE 'Fernando Santos';
+
+--7
+SELECT player FROM goal JOIN game ON matchid = id
+WHERE stadium = 'National Stadium, Warsaw';
+
+--8
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE (teamid !='GER' AND team1='GER') OR (teamid !='GER' AND team2='GER');
+
+--9
+SELECT teamname, COUNT(player)
+  FROM eteam JOIN goal ON id=teamid
+  GROUP BY teamname;
+
+--10
+SELECT stadium, COUNT(player) 
+FROM game JOIN goal ON id = matchid
+GROUP BY  stadium;
+
+--11
+SELECT matchid,mdate, count(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+ GROUP BY mdate, matchid;
+ 
+--12
+SELECT matchid,mdate, count(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (teamid = 'GER')
+ GROUP BY mdate, matchid;
+
+ --13
+ SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1, team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game JOIN goal ON matchid = id
+  GROUP BY mdate, team1, team2
+  ORDER BY mdate, goal.matchid, team1, team2
+
