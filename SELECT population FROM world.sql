1c3e69e6bf4d1fc206d21704fa0dc554483dd0a6
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-18 20:04:28 -0600

    Add Select names

--- SELECT population FROM world.sql
+++ SELECT population FROM world.sql
@@ -11,7 +11,69 @@ SELECT name, population FROM world
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000;
 
+--SELECT names
+--1
+SELECT name FROM world
+  WHERE name LIKE 'Y%';
 
+--2
+SELECT name FROM world
+  WHERE name LIKE '%Y';
 
+--3
+SELECT name FROM world
+  WHERE name LIKE '%X%';
 
+--4
+SELECT name FROM world
+  WHERE name LIKE '%land';
+
+--5
+SELECT name FROM world
+  WHERE name LIKE 'c%ia';
+
+--6
+SELECT name FROM world
+  WHERE name LIKE '%oo%';
+
+--7
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
+
+--8
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name;
+
+--9
+SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+
+--10
+SELECT name FROM world
+ WHERE name LIKE '____';
+
+--11
+SELECT name
+  FROM world
+ WHERE name = capital;
+
+--12
+SELECT name
+  FROM world
+ WHERE concat(name, ' City') = Capital;
+
+--13
+SELECT capital, name
+FROM world
+WHERE capital like concat('%', name, '%');
+
+--14
+SELECT capital, name
+FROM world
+WHERE capital like concat('%', name, '%') AND len(capital) > len(name);
+
+--15
+SELECT name, REPLACE(capital, name, '') FROM world
+WHERE capital like concat(name, '%')AND len(capital) > len(name);
 
