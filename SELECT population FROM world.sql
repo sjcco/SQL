e8c66d2080526f389c0a2e94c4373172ce8e50e3
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-18 20:05:28 -0600

    Add select from world tutorial

--- SELECT population FROM world.sql
+++ SELECT population FROM world.sql
@@ -77,3 +77,66 @@ WHERE capital like concat('%', name, '%') AND len(capital) > len(name);
 SELECT name, REPLACE(capital, name, '') FROM world
 WHERE capital like concat(name, '%')AND len(capital) > len(name);
 
+--SELECT form WORLD Tutorial
+
+--1
+SELECT name, continent, population FROM world;
+
+--2
+SELECT name
+  FROM world
+ WHERE population >= 200000000;
+
+ --3
+SELECT name, gdp/population
+  FROM world
+ WHERE population >= 200000000;
+
+ --4
+ SELECT name, population/1000000 FROM world
+WHERE continent LIKE 'South America';
+
+--5
+SELECT name, population FROM world
+  WHERE name IN ('France', 'Germany', 'Italy');
+
+--6
+SELECT name FROM world
+WHERE name LIKE 'United%';
+
+--7
+SELECT name, population, area FROM world
+WHERE population > 250000000 OR area > 3000000;
+
+--8
+SELECT name, population, area FROM world
+WHERE (area > 3000000 AND population < 250000000)
+    OR (area < 3000000 AND population > 250000000);
+
+--9
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world;
+WHERE continent LIKE 'South America';
+
+--10
+SELECT name, ROUND(gdp/population, -3) FROM  world
+WHERE gdp >= 1000000000000;
+
+--11
+SELECT name, capital
+  FROM world
+ WHERE LEN(name) = LEN(capital);
+
+--12
+SELECT name, capital
+FROM world
+WHERE capital LIKE concat(LEFT(name,1), '%') AND capital <> name;
+
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+  AND name LIKE '%e%'
+  AND name LIKE '%i%'
+  AND name LIKE '%o%'
+  AND name LIKE '%u%'
+  AND name NOT LIKE '% %';
+
