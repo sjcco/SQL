5ea97aad6f50ea78a5807c9ca0602a523de9703b
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-20 10:34:52 -0600

    Add self join

--- SELECT population FROM world.sql
+++ SELECT population FROM world.sql
@@ -505,4 +505,57 @@ CASE
      WHEN dept = 3 THEN 'Art'
      ELSE 'None'
 END AS 'Branch'
-FROM teacher
+FROM teacher;
+
+--Self join
+--1
+SELECT COUNT(name) FROM stops;
+
+--2
+SELECT id FROM stops
+WHERE name LIKE 'Craiglockhart';
+
+--3
+SELECT id, name FROM route JOIN stops ON stop=id
+WHERE num=4 AND company='LRT';
+
+--4
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) = 2
+
+--5
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = (SELECT DISTINCT id FROM stops JOIN route ON  id = stop
+WHERE name = 'London Road');
+
+--6
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road';
+
+--7
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON a.num = b.num
+WHERE a.stop = 115 AND b.stop = 137;
+
+--8
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross';
+
+--9
+SELECT DISTINCT name, a.company, a.num
+FROM route a
+JOIN route b ON (a.company = b.company AND a.num = b.num)
+JOIN stops ON a.stop = stops.id
+WHERE b.stop = 53;
