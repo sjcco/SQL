96cb2ef50290f5845a63dcbb195d1abd1b181955
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-20 09:48:50 -0600

    Add using null

--- SELECT population FROM world.sql
+++ SELECT population FROM world.sql
@@ -452,3 +452,57 @@ WHERE movieid IN (
 SELECT movieid FROM casting
 WHERE actorid=(SELECT id FROM actor WHERE name='Art Garfunkel')
 ) AND name!='Art Garfunkel';
+
+--Using Null
+--1
+SELECT name FROM  teacher
+WHERE dept IS NULL;
+
+--2
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id);
+
+--3
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id);                   
+
+--4
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id);
+
+--5
+SELECT name, COALESCE(mobile, '07986 444 2266') FROM teacher;
+
+--6
+SELECT teacher.name, COALESCE(dept.name, 'None')
+       FROM teacher LEFT JOIN dept
+                 ON (teacher.dept = dept.id);
+
+--7
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher;
+
+--8
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept
+GROUP BY dept.name;
+
+--9
+SELECT name,
+CASE 
+     WHEN dept = 1 OR dept = 2 THEN 'Sci'
+     ELSE 'Art'
+END AS 'Branch'
+FROM teacher;
+
+--10
+SELECT name,
+CASE 
+     WHEN dept = 1 OR dept = 2 THEN 'Sci'
+     WHEN dept = 3 THEN 'Art'
+     ELSE 'None'
+END AS 'Branch'
+FROM teacher
